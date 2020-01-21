2b439cfa446a5ce3430893ec0e772c779d0265be
soundof5ilence <54803214+soundof5ilence@users.noreply.github.com>
2020-01-21 11:24:38 +0100

    Dit is mijn opdr 4 van scr. dit is het animeren van circles.

--- 04_anim/script.js
+++ 04_anim/script.js
@@ -10,44 +10,53 @@ const height = window.innerHeight;
 canvas.width = width;
 canvas.height = height;
 
+let CircleObject = {};
+let CircleArray = [];
 
-let circles = [];
-
-for(let i = 0; i<10; i ++)
-
-// start Circle
-
-let circleObject = {};
-circleObject,x = 300;
-circleObject,y = 400;
-circleObject.radius = 40;
-circleObject.color = "purple";
-circleObject.speedX = 3;
-circleObject.speedY = 4;
-
-circleObject.draw = function(){
-context.beginPath();
-context.fillstyle = "purple"
-context.arc(circleObject.x,circleObject.y,circleObject.radius,0,2*Math.PI)
-
-context.stroke();
-context.fill();
+function getRandomInt(min,max){
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random(min) * max);
 }
-circleObject.update = function(){
-    circleObject.x = circleObject.x + circleObject.speedX;
-    circleObject.y = circleObject.y + circleObject.speedY ;
-    if(circleObject.speedY = - circleObject.speedY);
-}
- if(circleObject.x > width - circleObject.radius || circleObject.x < circleObject.radius){
-circleObject.speedX = - circleObject.speedX;
- 
 
- }
+for (let i = 0;i<10;i++){
+  let CircleObject = {x:getRandomInt(0,width),y:getRandomInt(0,height),radius:50,color:"rgb("+getRandomInt(100,500)+","+getRandomInt(240,255)+","+getRandomInt(0,0)+")",velocity_x:50,velocity_y:70}
+  if (Math.random(0)*10 > 5){
+    CircleObject.velocity_x = - CircleObject.velocity_x;
+  }if (Math.random(0)*10 > 5){
+    CircleObject.velocity_y = - CircleObject.velocity_y;
+  }
+  CircleObject.draw = function(){
+    context.beginPath();
+    context.arc(this.x,this.y,this.radius,0,2*Math.PI);
+    context.closePath();
+    context.strokeStyle = this.color;
+    context.stroke();
+    context.fillStyle = this.color;
+    context.fill();
+  }
+
+  CircleObject.update = function(){
+    CircleObject.x += CircleObject.velocity_x;
+    CircleObject.y += CircleObject.velocity_y;
+    CircleObject.radius = getRandomInt(0,15);
+    if (CircleObject.x > width - CircleObject.radius || CircleObject.x < CircleObject.radius){
+      CircleObject.velocity_x = - CircleObject.velocity_x;
     }
-    function loop(){
-        context.clearRect(0,0,width)
+    if (CircleObject.y > height - CircleObject.radius || CircleObject.y < CircleObject.radius){
+      CircleObject.velocity_y = - CircleObject.velocity_y;
     }
+  }
+  CircleArray.push(CircleObject);
+  console.log(CircleArray[i]);
 }
 
-circleObject.draw();
+function loop(){
+  //context.clearRect(0,0,width,height);
+  for (let i = 0;i<CircleArray.length;i++){
+    CircleArray[i].update();
+    CircleArray[i].draw();
+  }
 }
+
+setInterval(loop, 10);
